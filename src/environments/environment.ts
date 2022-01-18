d5c2d217b75f743295aa0103fb35d900ee76726e
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-01-18 14:40:46 -0300

    inicio do projeto com firebase configurado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'filmflix-b667b',
+    appId: '1:315045048926:web:7314572e215874e0873acf',
+    storageBucket: 'filmflix-b667b.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCD_WSoCv2dTwx88Cadx3C4NA6jPvU4kHA',
+    authDomain: 'filmflix-b667b.firebaseapp.com',
+    messagingSenderId: '315045048926',
+  },
   production: false
 };
 
